7711f8da7fa47e2509b6de03d8cbcbbe3f42dc83
nabi <nabisobhi@gmail.com>
2021-08-28 19:49:47 +0430

    update the solution to .net 5

--- VacationRental.Api/Startup.cs
+++ VacationRental.Api/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 using VacationRental.Api.Data;
 using VacationRental.Api.Domain;
@@ -37,10 +39,10 @@ namespace VacationRental.Api
             IMapper mapper = mapperConfig.CreateMapper();
             services.AddSingleton(mapper);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
 
             services.AddSwaggerGen(opts => {
-                opts.SwaggerDoc("v1", new Info
+                opts.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "Vacation rental information",
                     Version = "v1"
@@ -59,14 +61,19 @@ namespace VacationRental.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             app.UseSwagger();
             app.UseSwaggerUI(opts => opts.SwaggerEndpoint("/swagger/v1/swagger.json", "VacationRental v1"));
         }
