53e436a56018ca197d6d98e4762360994e861345
nabi <nabisobhi@gmail.com>
2021-08-29 19:46:36 +0430

    add unit test for validators

---
+++ VacationRental.Api.UnitTests/Validators/RentalBindingModelVaidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using VacationRental.Api.Validators;
+using Xunit;
+
+namespace VacationRental.Api.UnitTests.Validators
+{
+    [Collection("Validators")]
+    public class RentalBindingModelVaidatorTests
+    {
+        private readonly RentalBindingModelVaidator _validator;
+
+        public RentalBindingModelVaidatorTests()
+        {
+            _validator = new RentalBindingModelVaidator();
+        }
+
+        public static IEnumerable<object[]> Data => new[]
+            {
+                new object[] { 1, 0, true },
+                new object[] { 0, 1, false },
+                new object[] { -1, 1, false },
+                new object[] { 1, -1, false },
+                new object[] { 1, 1, true },
+            };
+
+        [Theory, MemberData(nameof(Data))]
+        public void GivenData_WhenCallValidator_ThenCheckResult(int units, int preparationTimeInDays, bool expectedResult)
+        {
+            Assert.Equal(expectedResult, _validator.Validate(new Models.RentalBindingModel
+            {
+                Units = units,
+                PreparationTimeInDays = preparationTimeInDays,
+            }).IsValid);
+        }
+    }
+}
